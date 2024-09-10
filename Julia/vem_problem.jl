4204124f2a79db59bd7f870bc40535ee02e50d11
Rory Yarr <roryyarr314@gmail.com>
2024-09-10 23:00:11 +1000

    Cleaned vem_problem

--- Julia/vem_problem.jl
+++ Julia/vem_problem.jl
@@ -2,16 +2,14 @@ module vem_problem
 
 using SparseArrays
 
-export VEMProblem, Mesh
+# Export the Mesh struct so it can be used outside the module
+export Mesh
 
-struct VEMProblem
-    # Add fields as per your problem's structure
-end
-
+# Define the Mesh struct
 struct Mesh
     boundary::Vector{Int64}    # Indices of boundary vertices
     elements::Vector{Vector{Int64}}  # Indices of vertices for each element (e.g., triangle or polygon)
     vertices::Vector{Tuple{Float64, Float64}}  # Coordinates of vertices
 end
 
-end # module
+end # End of the module
